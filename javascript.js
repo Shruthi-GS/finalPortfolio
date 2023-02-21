f78973564b079115750159bc66cf6586716856b6
Shruthi G S <116060026+Shruthi-GS@users.noreply.github.com>
2023-02-22 00:20:38 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-// add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 console.log(header)
 window.onscroll = function() {
@@ -10,10 +9,44 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
-// collapse navbar after click on small devices
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarSupportedContent')
 
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+function validateForm()
+{  
+var x=document.myForm.email.value;  
+var atposition=x.indexOf("@");  
+var dotposition=x.lastIndexOf(".");  
+if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+  alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+  return false;  
+  }
+  else{
+	let x=document.forms["myForm"]["fname"].value;
+	if(x=="")
+	{
+		alert("Name must be filled out");
+		return false;
+		}
+        let y=document.forms["myForm"]["email"].value;
+        if(y=="")
+		{
+		alert("Email must be filled out");
+		return false;
+		}
+        let z=document.forms["myForm"]["phone"].value;
+        if(z=="")
+		{
+		alert("Phone number must be filled out");
+		return false;
+		}
+		else
+		{
+		alert("Submitted successfully!");
+		return true;
+		}
+	}
+	}
